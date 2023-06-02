fda27ad586eda67465b7111541c1206772371dd6
AVINASH KUMAR <65335071+avipandit@users.noreply.github.com>
2023-06-02 12:17:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,22 +2,27 @@ const secondHand = document.querySelector('.second-hand');
 const minsHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
 
- function setDate() {
-  const now = new Date();
 
-  const seconds = now.getSeconds();
-  const secondsDegrees = ((seconds / 60) * 360) + 90;
-  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+function setDate() {
+    const now = new Date();
+    
 
-  const mins = now.getMinutes();
-  const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
-  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+    const seconds = now.getSeconds();
+    const mins = now.getMinutes();
+    const hour = now.getHours();
 
-  const hour = now.getHours();
-  const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
-  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+    console.log("curr", now, seconds, mins, hour)
+
+    const hourDegrees = (30*hour + mins/2)%360;
+    const minsDegrees = 6*mins;
+    const secondsDegrees = 6*seconds;
+
+
+    console.log('degres', hourDegrees, minsDegrees, secondsDegrees)
+    
+    secondHand.style.transform = `rotate(${secondsDegrees + 90}deg)`;
+    minsHand.style.transform = `rotate(${minsDegrees + 90}deg)`;
+    hourHand.style.transform = `rotate(${hourDegrees +90 }deg)`;
 }
 
 setInterval(setDate, 1000);
-
-setDate();
